7c91096be6d6dad64398afdeafe8483e3002bf5d
avish <avishmadushanka21@gmail.com>
2024-07-01 14:09:42 +0530

    update app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import login from './pages/login';
 import './App.css';
 
 function App() {
@@ -6,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/" element={<login />} />
         <a
           className="App-link"
           href="https://reactjs.org"
