73f0e3073e4efd8bd217905bed10ce1be97f650e
avish <avishmadushanka21@gmail.com>
2024-07-15 21:57:58 +0530

    update app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import Login from './pages/login';  // Ensure the file name is 'Login.jsx'
+import Login from './pages/Login';  // Ensure the file name is 'Login.jsx'
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Router>
           <Routes>
-            <Route path="/" element={<login />} />
+            <Route path="/" element={<Login />} />
           </Routes>
         </Router>
         <a
